68ee7fa08d478bbb95cc687b6670bbe89fec8b33
Daniel Montero <d.montero95@gmail.com>
2018-03-05 11:03:13 -0600

    Proyecto en Visual para pruebas del juego en consola.
    05/03/2018 11:01
    
    Se creó un proyecto de C# en Visual para realizar las funciones necesarias para la funcionalidad del juego.
    
    Co-Authored-By: Adrian Solis <solaz92@gmail.com>
    Co-Authored-By: xragex <xragex@users.noreply.github.com>

---
+++ Pruebas/PruebaJuegoConsola/PruebaJuegoConsola/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaJuegoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Juego juego = new Juego(8);
+            juego.iniciarMatriz();
+            juego.dibujar();
+
+            List<List<int>> movidas = juego.MovidasPosibles(1);
+            Console.Write(movidas);
+            Console.ReadLine();
+
+
+        }
+    }
+}
