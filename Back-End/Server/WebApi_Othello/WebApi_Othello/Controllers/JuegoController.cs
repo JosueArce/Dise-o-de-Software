d02e0d5e52b342a5cc5af0c8cb7230d6c0898d02
Daniel Montero <d.montero95@gmail.com>
2018-04-18 13:50:13 -0600

    18/4/18
    Inicio de conexión de aplicación con el servidor para los modos Sistema vs Sistema y Sistema vs Jugador
    
    Co-Authored-By: Adrian Solis <solaz92@gmail.com>
    Co-Authored-By: xragex <xragex@users.noreply.github.com>

---
+++ Back-End/Server/WebApi_Othello/WebApi_Othello/Controllers/JuegoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApi_Othello.Models;
+
+namespace WebApi_Othello.Controllers
+{
+    public class JuegoController : Controller
+    {
+        private Juego juego;
+
+        public JuegoController()
+        {
+            this.juego = new Juego(0, 0);//no final
+        }
+
+
+        
+        // GET: Juego
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
